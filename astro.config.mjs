c6096b89a5892952e8d72ce85bfe5d2ce7ee938e
lukpan <118743051+lukpan@users.noreply.github.com>
2025-11-30 20:53:51 +1100

    small updates to allow deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   // Update this to your final domain when you get one
   // For GitHub Pages, it will be https://[your-username].github.io/[repo-name]
-  site: 'https://lukpan.github.io',
-  base: '/portfolio', // Change this to your repo name
+  site: 'https://www.lukepanaccio.com',
+  // base: '/portfolio', // Change this to your repo name
   integrations: [
     mdx(),
     sitemap(),
